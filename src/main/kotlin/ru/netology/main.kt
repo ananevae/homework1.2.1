38544c176e75b7e7e6d2423d4f0740d16b8373d8
ananevae <ananeva.ea@yandex.ru>
2022-03-21 10:58:11 +1000

    First commit

---
+++ src/main/kotlin/ru/netology/main.kt
@@ -0,0 +1,11 @@
+package ru.netology
+
+fun main() {
+    val amount = 10000_00
+    val commissionPerc = 0.0075
+    val minComission = 35_00
+
+    val commission = amount * commissionPerc
+    val totalCommission = if (commission > minComission) commission.toInt() else minComission
+    println("Total Commission: " + totalCommission / 100 + " руб. " + totalCommission % 100 + " коп.")
+}
